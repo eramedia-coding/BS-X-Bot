e3b2302fc48c8abe2f418a38e6224ad1379f3059
TailsEraYT <43537784+TailsEraYT@users.noreply.github.com>
2019-09-02 15:31:11 -0400

    Update Satellaplay.js

--- events/Satellaplay.js
+++ events/Satellaplay.js
@@ -17,7 +17,7 @@ const embed = {
       "value": "1.) Download the BS-X BIOS. (https://project.satellaview.org/download/BS-X%20BIOS%20(English)%20[No%20DRM]%20[2016%20v1.3].sfc) \n2.) Download your SNES emulator of choice. (SNES9x is recommended.) (https://github.com/snes9xgit/snes9x/releases/download/1.60/snes9x-1.60-win32-x64.zip)\n3.) Click the arrow near the X button.\n4.) Click 'Map Network Drive'\n5.) Mount this URL to Drive (satellaplay.ddns.net)\n6.) Download the latest broadcast data (Avalible In the Drive)\n7.) Extract and place into the BS-X data folder (SatData for SNES9x)\n8.) Launch the BIOS.\n9.) Enjoy!"
     },
     {
-      "name": "WARNING:"
+      "name": "WARNING:",
       "value": "This Service is only in the US."
     },
     {
